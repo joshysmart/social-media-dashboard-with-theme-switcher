44f8d8951133097f503b426b1456cd74f8d9f396
Joshysmart <sanijoshua10@gmail.com>
2020-09-01 01:20:46 +0100

    Updates

---
+++ precache-manifest.04964568cad39f20a7ee4c35f47c53bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a6bc048b62176ab725dbd1972f96dc",
+    "url": "/social-media-dashboard-with-theme-switcher/index.html"
+  },
+  {
+    "revision": "0dc57928c8927fedb040",
+    "url": "/social-media-dashboard-with-theme-switcher/static/css/main.849552cd.chunk.css"
+  },
+  {
+    "revision": "a53c40ed6787a7b14a85",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/2.0467fe2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/2.0467fe2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc57928c8927fedb040",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/main.36652352.chunk.js"
+  },
+  {
+    "revision": "75c0202dfc535eeb430d",
+    "url": "/social-media-dashboard-with-theme-switcher/static/js/runtime-main.9cda8c86.js"
+  }
+]);
